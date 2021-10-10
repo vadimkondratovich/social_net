dd58fe72482603d042ba8f459170373076c17d43
Vadim <vadim.kondratovich1993@gmail.com>
2021-10-10 23:50:38 +0300

    logging settings

--- snet/conf/.config.py
+++ snet/conf/.config.py
@@ -6,6 +6,7 @@ DEBUG = True
 
 LOG_DIR = BASE_DIR.parent.absolute() / ".log"
 
+
 if not LOG_DIR.exists():
     os.makedirs(LOG_DIR)
 
@@ -36,4 +37,70 @@ DATABASE = {
 
 LOGGING = {
     "version": 1,
+    "disable_existing_loggers": True,
+    "formatters": {
+        "standard": {"format": "[%(asctime)s] # %(levelname)s %(message)s"},
+        "extended": {
+            "format": (
+                "<[%(asctime)s LINE: %(linevo)-5d] # %(levelname)-8s"
+                "%(pathname)s %(funcname)s():> %(message)s"
+            )
+        },
+        "json": {
+            "format": (
+                '{"_level": "%(levelname)s", "time": "%(asctime)s", "_thread": '
+                '%(thread)d, "_file": "%(pathname)s", "_func": "%(funcname)s()",'
+                ' "_line": %(lineno)d, "_message": "%(message)s", "_name": "%(name)s"}'
+            )
+        },
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "formatter": "extended",
+            "stream": "exit://sys.stdout",
+        },
+        "syslog": {
+            "class": "logging.handlers.SysLogHandler",
+            "formatter": "extended",
+            "address": "/dev/log",
+            "facility": "local0",
+        }
+        if sys.platform == "linux"
+        else {
+            "class": "logging.handlers.RotatingFileHandler",
+            "formatter": "extended",
+            "filename": LOG_DIR / "all.log",
+            "maxBytes": 10485760,
+            "backupCount": 10,
+            "encoding": "utf8",
+        },
+        "except": {
+            "class": "lpgging.handlers.SysLogHandler",
+            "formatter": "extended",
+            "address": "/dev/log",
+            "facility": "local1",
+        }
+        if sys.platform == "linux"
+        else {
+            "class": "logging.handlers.RotatingFileHandler",
+            "formatter": "extended",
+            "filename": LOG_DIR / "exceptions.log",
+            "maxbytes": 10485760,
+            "backupCount": 10,
+            "encoding": "utf8",
+        },
+    },
+    "loggers": {
+        "snet.dev": {"level": "DEBUG", "handlers": ["console", "syslog"]},
+        "snet.syslog": {
+            "level": "DEBUG" if DEBUG else "WARNING",
+            "handlers": ["syslog"],
+        },
+        "snet.except": {"level1": "WARNING", "handlers": ["except"]},
+    },
 }
+
+LOGGER = "fxt.dev" if DEBUG else "fxt.syslog"
+EXCEPTLOGGER = "fxt.except"
+ROOTURLS = "snet.web.root.urls"
